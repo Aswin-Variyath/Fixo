129112bae368bbc9dc5e0bb5abf7020e2f2d341b
Aswin <aswinv793@gmail.com>
2026-08-04 20:53:15 +0530

    feat(auth): implement signup page

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        loadComponent:()=>
+            import("./features/landing/customer/home.component").then((m)=>m.HomeLanding)
+    },
+    {
+        path:"signup",
+        loadComponent:()=>
+            import("./features/auth/signup/signup.component").then((m)=>m.SignupComponent)
+
+    }
+];
