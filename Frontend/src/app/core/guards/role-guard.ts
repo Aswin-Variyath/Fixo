d06a5abf0fc275dc5155ee0d83093989eb5b954a
Aswin <aswinv793@gmail.com>
2026-08-21 22:08:07 +0530

    error:roleGuard spec file error fixed

--- Frontend/src/app/core/guards/role-guard.ts
+++ Frontend/src/app/core/guards/role-guard.ts
@@ -2,24 +2,29 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../auth/auth.service';
 
-export const roleGuard = (allowedRole:string): CanActivateFn => {
-
+export const roleGuard = (allowedRole: string): CanActivateFn => {
   return () => {
-    const authService = inject(AuthService)
-    const router = inject(Router)
+    const authService = inject(AuthService);
+    const router = inject(Router);
 
-    const user = authService.user()
-    console.log("asdfakjsdf",user?.activeRole.type)
-    if(!user) {
-      return router.createUrlTree(['/login'])
+    const user = authService.user();
+
+    if (!user) {
+      return router.createUrlTree(['/login']);
     }
 
-    if(user.activeRole.type === allowedRole) return true
+    if (user.activeRole.type === allowedRole) {
+      return true;
+    }
 
-    if(user.activeRole.type === 'customer') return router.createUrlTree(['/customer/home'])
-    
-    if(user.activeRole.type === 'tasker') router.createUrlTree(['/tasker/home'])
-    
-      return router.createUrlTree(['/'])
-  }
+    if (user.activeRole.type === 'customer') {
+      return router.createUrlTree(['/customer/home']);
+    }
+
+    if (user.activeRole.type === 'tasker') {
+      return router.createUrlTree(['/tasker/home']);
+    }
+
+    return router.createUrlTree(['/']);
+  };
 };
