352a238ca6dd5e9c60800a4707e2d039c0aec4ab
Aswin <aswinv793@gmail.com>
2026-09-05 19:50:03 +0530

    feat(category): integrate categories into customer home

--- Frontend/src/app/features/customer/home/sections/services/services.ts
+++ Frontend/src/app/features/customer/home/sections/services/services.ts
@@ -1,19 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
+
 import { CategoryService } from '../../categories/category.service';
+import { CategoryStore } from '../../categories/category.store';
 
 @Component({
-  selector: 'app-services',
-  imports: [],
-  templateUrl: './services.html',
-  styleUrl: './services.css',
+    selector: 'app-services',
+    imports: [],
+    providers: [CategoryService, CategoryStore],
+    templateUrl: './services.html',
+    styleUrl: './services.css',
 })
-export class Services implements OnInit{
-  private readonly categoryService = inject(CategoryService)
+export class Services implements OnInit {
+  private readonly categoryService = inject(CategoryService);
 
-  readonly catgories = this.categoryService.categories
-  readonly isLoading = this.categoryService.isLoading
-  readonly error = this.categoryService.error
-  ngOnInit(): void {
-    this.categoryService.loadCategories().subscribe()
-  }
+    readonly categories = this.categoryService.categories;
+    readonly isLoading = this.categoryService.isLoading;
+    readonly error = this.categoryService.error;
+
+    ngOnInit(): void {
+        this.categoryService.loadCategories().subscribe();
+    }
+
+    retryLoadingCategories(): void {
+        this.categoryService.loadCategories().subscribe();
+    }
 }
